87e117b0ef0b57a15ad35a6e52a13d8e4cb740a8
Thiago <ThigsMendes222>
2026-04-30 21:13:59 -0300

    docs: ajuste final README

--- src/app.py
+++ src/app.py
@@ -1,7 +1,22 @@
 tasks = []
 
 def create_task(title):
-    tasks.append(title)
+    tasks.append({"title": title, "done": False})
 
 def list_tasks():
     return tasks
+
+def update_task(index, new_title):
+    if 0 <= index < len(tasks):
+        tasks[index]["title"] = new_title
+
+def complete_task(index):
+    if 0 <= index < len(tasks):
+        tasks[index]["done"] = True
+
+def delete_task(index):
+    if 0 <= index < len(tasks):
+        tasks.pop(index)
+
+
+        
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,25 @@
-from src.app import create_task, list_tasks
+from src.app import create_task, list_tasks, update_task, complete_task, delete_task, tasks
+import pytest
+
+@pytest.fixture(autouse=True)
+def clear_tasks():
+    tasks.clear()
 
 def test_create_task():
     create_task("Teste")
-    assert "Teste" in list_tasks()
+    assert list_tasks()[0]["title"] == "Teste"
 
-    
+def test_update_task():
+    create_task("Antigo")
+    update_task(0, "Novo")
+    assert list_tasks()[0]["title"] == "Novo"
+
+def test_complete_task():
+    create_task("Fazer algo")
+    complete_task(0)
+    assert list_tasks()[0]["done"] == True
+
+def test_delete_task():
+    create_task("Apagar")
+    delete_task(0)
+    assert len(list_tasks()) == 0
